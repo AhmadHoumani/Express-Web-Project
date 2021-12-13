039fefe1299039fbbddd725886bbc1412d91c7b8
Ahmad Houmani <31387896+AhmadHoumani@users.noreply.github.com>
2021-12-12 17:14:22 -0800

    Add files via upload

---
+++ controller/shop.js
@@ -0,0 +1,17 @@
+exports.addToCart = (req, res, next) => {
+    req.user.addToCart(req.body.id)
+        .then(() => {
+            res.redirect('/cart');
+        }).catch(err => console.log(err));
+}
+
+exports.getCart = (req, res, next) => {
+    req.user
+        .populate('cart.items.productId')
+        .execPopulate()
+        .then(user => {
+            console.log(user);
+            res.render('cart', { cart: user.cart, pageTitle: 'Shopping Cart Detail', path: '/cart', name: 'Edward' });
+        })
+        .catch(err => console.log(err));
+}
